58c3666fcb887dfb34be0dc5eaf5562368f7779b
hady-awayda <hady.awayda@gmail.com>
2024-07-28 00:57:06 +0300

    Created models

---
+++ app/Models/Conversation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conversation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["message", "user_id_1", "user_id_2"];
+    
+    public function user1()
+    {
+        return $this->belongsTo(User::class, 'user_id_1');
+    }
+
+    public function user2()
+    {
+        return $this->belongsTo(User::class, 'user_id_2');
+    }
+}
