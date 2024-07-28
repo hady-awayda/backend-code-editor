da0a5278ea5ff08c46af2fca7c73b7dfb15609cf
hady-awayda <hady.awayda@gmail.com>
2024-07-28 03:43:31 +0300

    Added Controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,13 +6,11 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
-use App\Http\Services\UserService;
 
 class UserController extends Controller
 {
     public function getAllUsers() {
         $users = User::all();
-        
         return response()->json([
             'data' => $users
         ], 200);
@@ -20,52 +18,65 @@ class UserController extends Controller
     
     public function getUserById($user_id) {
         $user = User::find($user_id);
-
         return response()->json([
             'data' => $user
         ], 200);
     }
 
     public function createUser(Request $request) {
-        $response = UserService::createUser($request);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
 
-        if ($response == "success") {
+        if ($validator->fails()) {
             return response()->json([
-                'message' => 'Created Successfully'
-            ], 201);
-        } else {
-            return response()->json([
-                "errors" => $response
+                'errors' => $validator->errors()
             ], 422);
         }
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json([
+            "message" => 'Created Successfully'
+        ], 201);
     }
 
     public function updateUser(Request $request, $user_id) {
-        $response = UserService::updateUser($request, $user_id);
-        
-        if ($response == "success") {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
             return response()->json([
-                'message' => 'Updated Successfully'
-            ], 200);
-        } else {
-            return response()->json([
-                "errors" => $response
+                'errors' => $validator->errors()
             ], 422);
         }
+        
+        $user = User::find($user_id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        
+        return response()->json([
+            "message" => 'Updated Successfully'
+        ]);
     }
 
     public function deleteUser($id)
     {
-        $response = UserService::deleteUser($id);
-
-        if ($response === "success") {
-            return response()->json([
-                "message" => 'Deleted Successfully'
-            ]);
-        } else {
-            return response()->json([
-                "message" => $response
-            ], 404);
-        }
+        $user = User::find($id);
+        $user->delete();
+        return response()->json([
+            "message" => 'Deleted Successfully'
+        ]);
     }
 }
