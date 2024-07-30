742e511dcb84f0478f9f962238b8e6501018c47c
hady-awayda <hady.awayda@gmail.com>
2024-07-30 21:44:53 +0300

    Fixed authentication return to include an authorization key

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,7 +38,7 @@ class AuthController extends Controller
         return response()->json([
                 'status' => 'success',
                 'user' => $user,
-                'authorisation' => [
+                'authorization' => [
                     'token' => $token,
                     'type' => 'bearer',
                 ]
@@ -70,7 +70,7 @@ class AuthController extends Controller
             'status' => 'success',
             'message' => 'User created successfully',
             'user' => $user,
-            'authorisation' => [
+            'authorization' => [
                 'token' => $token,
                 'type' => 'bearer',
             ]
@@ -92,7 +92,7 @@ class AuthController extends Controller
         return response()->json([
             'status' => 'success',
             'user' => Auth::user(),
-            'authorisation' => [
+            'authorization' => [
                 'token' => Auth::refresh(),
                 'type' => 'bearer',
             ]
