6c883fc33186f82f043884b1954ba10fda11618d
hady-awayda <hady.awayda@gmail.com>
2024-07-31 13:01:06 +0300

    Fixed Search Controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,7 +9,19 @@ class SearchController extends Controller
 {
     public function search($username)
     {
-        $users = User::where('name', 'like', '%' . $username . '%')
+        $validator = Validator::make(['username' => $username], [
+            'username' => 'required|string|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => $validator->errors()
+            ], 422);
+        }
+
+        $name = $validator->validated()['username'];
+        
+        $users = User::where('name', 'like', '%' . $name . '%')
         ->select('id', 'name')
         ->get();
 
