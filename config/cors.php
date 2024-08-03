0dbe2f10c5f18960e9489b91430d3b0dfd9a7c3f
Hady <hady.awayda@gmail.com>
2024-08-04 02:46:16 +0300

    Added new route to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'http://localhost:5173'],
+    'allowed_origins' => ['*', 'http://35.180.202.75', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
